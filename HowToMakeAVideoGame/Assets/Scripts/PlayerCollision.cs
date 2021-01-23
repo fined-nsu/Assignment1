2d019d34d1651b68c3c86ad128dbc19c4ea76e3f
fined-nsu <71229695+fined-nsu@users.noreply.github.com>
2021-01-22 23:18:53 -0600

    E08 complete
    Completed E08.

---
+++ HowToMakeAVideoGame/Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+
+    public PlayerMovement movement;
+
+    // Start is called before the first frame update
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.gameObject.tag == "Obstacle")
+        {
+            movement.enabled = false;
+            FindObjectOfType<GameManager>().EndGame();
+
+        }
+    }
+}
